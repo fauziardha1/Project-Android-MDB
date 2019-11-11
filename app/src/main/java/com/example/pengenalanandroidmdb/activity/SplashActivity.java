7326e81fc98cedae9a96d14f9dafa73ab3d39b0a
Satrio <satrioarbi07@gmail>
2019-11-11 15:09:20 +0700

    ku tambahin splash screen, sama ngerubah design log in. manifest juga ku ganti yaa biar yg muncul duluan activity splash screen

---
+++ app/src/main/java/com/example/pengenalanandroidmdb/activity/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.pengenalanandroidmdb.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.pengenalanandroidmdb.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int WAKTU_SPLASH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, WAKTU_SPLASH);
+    }
+}
