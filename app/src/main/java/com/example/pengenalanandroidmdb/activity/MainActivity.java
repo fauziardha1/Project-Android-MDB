d2834ac644cf730106d7e4127fb8408e04103b73
fauziardha1 <fauziardasaputra@gmail.com>
2019-11-14 20:07:29 +0700

    menggabungkan feature cuaca dengan main

--- app/src/main/java/com/example/pengenalanandroidmdb/activity/MainActivity.java
+++ app/src/main/java/com/example/pengenalanandroidmdb/activity/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.pengenalanandroidmdb.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,14 +14,26 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.androdocs.httprequest.HttpRequest;
 import com.example.pengenalanandroidmdb.R;
 import com.example.pengenalanandroidmdb.model.DownloadImageFromInternet;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView inputUsername,inputPassword;
     private Button btnLogin;
     private ImageView imgView;
+    private String CITY = "Jakarta,ID";
+    private String API = "5743929c4a396e392506b1896ab261b0";
+    private TextView addressTxt, updated_atTxt, statusTxt, tempTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +41,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imgView = findViewById(R.id.imageView);
+        //weather
+        addressTxt = findViewById(R.id.address);
+        updated_atTxt = findViewById(R.id.updated_at);
+        statusTxt = findViewById(R.id.status);
+        tempTxt     = findViewById(R.id.temp);
 
-
+        //jalankan api cuaca
+        new weatherTask().execute();
 
     }
 
@@ -62,6 +82,54 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
+    // class and method untuk menggunakan api cuaca
+    class weatherTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        protected String doInBackground(String... args) {
+            String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            try
+
+            {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONObject sys = jsonObj.getJSONObject("sys");
+                JSONObject wind = jsonObj.getJSONObject("wind");
+                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+                long updatedAt = jsonObj.getLong("dt");
+                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                String temp = main.getString("temp") + "°C";
+
+                String weatherDescription = weather.getString("description");
+
+                String address = jsonObj.getString("name") + ", " + sys.getString("country");
+
+
+                /* Populating extracted data into our views */
+                addressTxt.setText(address);
+                updated_atTxt.setText(updatedAtText);
+                statusTxt.setText(weatherDescription.toUpperCase());
+                tempTxt.setText(temp);
+
+            }catch(
+                    JSONException ignored)
+
+            {
+            }
+        }
+
+    }
+    //akhir api cuaca
 
 
 
